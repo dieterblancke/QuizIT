2cfd76c85e9dfecb94ec2dd0d93e929c28a0290f
tijlatoui <tijl.atoui@outlook.com>
2020-03-21 13:51:05 +0100

    Commit for JS

--- resources/js/create.js
+++ resources/js/create.js
@@ -1,9 +1,12 @@
 "use strict";
 
+let quizQuestions = [];
+
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     document.querySelector("#questionForm").addEventListener("submit", createQuestion);
+    document.querySelector("#submitQuiz").addEventListener("click", submitQuiz);
 }
 
 function createQuestion(e) {
@@ -13,11 +16,12 @@ function createQuestion(e) {
     let answers = document.querySelector("#answers");
 
     if (question.value.length !== 0 && answers.value.length !== 0) {
+        let questionValue = question.value;
         let table = document.querySelector("#questions");
         let answerArray = answers.value.split("\n");
 
         let toAdd = "<tr>" +
-            "<td>" + question.value + "</td>" +
+            "<td>" + questionValue + "</td>" +
             "<td>" +
             "<ul>";
         for (let i = 0; i < answerArray.length; i++) {
@@ -26,6 +30,13 @@ function createQuestion(e) {
         toAdd += "</ul>" +
             "</td>";
 
+        let question = {
+            "question": questionValue,
+            "answers": answerArray
+        };
+
+        quizQuestions.push(question);
+
         table.innerHTML += toAdd;
         question.value = null;
         answers.value = null;
@@ -33,3 +44,10 @@ function createQuestion(e) {
         document.querySelector("#closeModel").click();
     }
 }
+
+function submitQuiz(e) {
+    e.preventDefault();
+
+    console.log(quizQuestions);
+}
+
