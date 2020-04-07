020a11ce3a7b03bef4355dbcb9f6690a212e0c4f
Dieter Blancke <dieter.blancke@outlook.com>
2020-04-07 09:51:44 +0200

    updated join form

--- resources/views/join.blade.php
+++ resources/views/join.blade.php
@@ -5,13 +5,13 @@
 @section('content')
     <h1>Join quiz</h1>
 
-    <form action="{{route("join")}}" method="POST" class="row">
-        <div class="col-6">
+    <form action="{{route("join")}}" method="POST">
+        <div>
             <label for="quizid">Quiz ID</label>
             <input type="text" min="0" name="quizid" id="quizid" class="form-control">
         </div>
-        <div class="col-6">
-            <input type="submit" value="QuizIN" class="btn btn-secondary">
+        <div>
+            <input type="submit" value="QuizIN" class="btn btn-primary">
         </div>
     </form>
 @endsection
